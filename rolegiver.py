c1094a61e57795248169007a0164e3ba641e773e
Nick Engle <enick11@gmail.com>
2019-05-11 23:11:56 -0400

    changed roles to reddit/4ch

--- rolegiver.py
+++ rolegiver.py
@@ -1,5 +1,7 @@
 import discord
 from mod import DispatchedBot
+loliname = 'reddit'
+shoutaname = '4chan'
 
 
 class RoleGiver(DispatchedBot):
@@ -11,17 +13,17 @@ class RoleGiver(DispatchedBot):
     async def on_message(self, client: discord.Client, game_data,
                          message: discord.Message):
         if self.loli is None:
-            self.loli = discord.utils.get(message.server.roles, name='loli')
+            self.loli = discord.utils.get(message.server.roles, name=loliname)
         if self.shouta is None:
-            self.shouta = discord.utils.get(message.server.roles, name='shouta')
+            self.shouta = discord.utils.get(message.server.roles, name=shoutaname)
         if message.content[:6] == '!role ':
             user_roles = message.author.roles
             if not (self.loli in user_roles and self.shouta in user_roles):
-                if message.content[6:] == 'loli':
+                if message.content[6:] == loliname:
                     await client.add_roles(message.author, self.loli)
-                    print(f'User {message.author.name} given role Loli')
+                    print(f'User {message.author.name} given role' + loliname)
                     await client.send_message(message.channel, 'Role given.')
-                if message.content[6:] == 'shouta':
+                if message.content[6:] == shoutaname:
                     await client.add_roles(message.author, self.shouta)
-                    print(f'User {message.author.name} given role Shouta')
+                    print(f'User {message.author.name} given role' + shoutaname)
                     await client.send_message(message.channel, 'Role given.')
