4dc6d293fe23ca6be085e971223f31a6ec648058
Nick Engle <enick11@gmail.com>
2020-04-12 01:02:13 -0400

    regex bugfix

--- music_kirbyer.py
+++ music_kirbyer.py
@@ -5,7 +5,7 @@ MUSIC_CHANNEL = ['451068243235307532']
 
 
 def is_valid_music_link(msg):
-    return re.match(r'.+https:\/\/www\.youtube\.com\/watch.+', msg) is not None
+    return re.match(r'.*https:\/\/www\.youtube\.com\/watch.*', msg) is not None
     
     
 class MusicKirbyer(DispatchedBot):
@@ -23,6 +23,7 @@ class MusicKirbyer(DispatchedBot):
         if message.channel.id in MUSIC_CHANNEL and is_valid_music_link(message.content):
             self.setup_kirby(message)
             await client.add_reaction(message, self.kirby)
+            print(f"User {message.user.id} posted new music.")
 
     # gives 1 music point to the user who posted the song
     async def on_reaction_add(self, client, game_data, reaction, user):
