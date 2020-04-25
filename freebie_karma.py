c3f46e7b70ee060f62fa0f03ee05b906e8e8a95e
Nick Engle <enick11@gmail.com>
2020-04-25 17:57:26 -0400

    fixed modbot getting freebies

--- freebie_karma.py
+++ freebie_karma.py
@@ -8,12 +8,15 @@ reaction_freebie_amt = 0.1
 class FreebieKarma(DispatchedBot):
     async def on_message(self, client: discord.Client, game_data,
                          message: discord.Message):
-        game_data.add_to_user_value(message.author.id, "karma", message_freebie_amt)
+        if user.id.id != client.user.id:
+            game_data.add_to_user_value(message.author.id, "karma", message_freebie_amt)
 
     async def on_reaction_add(self, client: discord.Client, game_data: GameData,
                               reaction: discord.Reaction, user: discord.User):
-        game_data.add_to_user_value(user.id, "karma", reaction_freebie_amt)
+        if user.id != client.user.id:
+            game_data.add_to_user_value(user.id, "karma", reaction_freebie_amt)
 
     async def on_reaction_remove(self, client: discord.Client, game_data: GameData,
                                  reaction: discord.Reaction, user: discord.User):
-        game_data.add_to_user_value(user.id, "karma", -reaction_freebie_amt)
+        if user.id != client.user.id:
+            game_data.add_to_user_value(user.id, "karma", -reaction_freebie_amt)
