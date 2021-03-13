94e90ba9cb05d087efce8407163e1c1ca257c78d
Nick Engle <enick11@gmail.com>
2021-03-13 14:56:15 -0500

    bugfix1

--- mccheck.py
+++ mccheck.py
@@ -7,7 +7,7 @@ IP_FILE = "../mc_server_ip.txt"
 class MCCheck(DispatchedBot):
     def __init__(self, *args, **kwargs):
         self.help = "!server - check MC server status"
-        self.server_ip = get_ip()
+        self.server_ip = self.get_ip()
         super().__init__(*args, **kwargs)
     
     async def on_message(self, client, game_data, message):
@@ -23,7 +23,7 @@ class MCCheck(DispatchedBot):
             except socket.timeout as timeout:
                 infostr = "Server Offline"
 
-    def get_ip():
+    def get_ip(self):
         with open(IP_FILE) as f:
             return str(f.read()) # idk if str is needed but dont feel like testing
 
